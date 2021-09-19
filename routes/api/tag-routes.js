05a09c5cf506b0c9937637e7ca69953eed78bdad
Nathan Flessner <nathanflessner1@gmail.com>
2021-09-18 21:01:17 -0600

    added put method to tag-routes.js

--- routes/api/tag-routes.js
+++ routes/api/tag-routes.js
@@ -15,7 +15,7 @@ router.get('/', async (req, res) => {
     }
   )
   if (!tagData) {
-    res.status(404).json({ message: 'No categories found!' });
+    res.status(404).json({ message: 'No tags found!' });
     return;
   }
   res.status(200).json(tagData);
@@ -49,12 +49,34 @@ router.get('/:id', async (req, res) => {
   }
 });
 
-router.post('/', (req, res) => {
+router.post('/', async (req, res) => {
   // create a new tag
+  try {
+    const tagData = await Tag.create(req.body);
+    res.status(200).json(tagData);
+  }
+  catch (err) {
+    res.status(400).json(err);
+  }
 });
 
-router.put('/:id', (req, res) => {
+router.put('/:id', async (req, res) => {
   // update a tag's name by its `id` value
+  try {
+    const tagData = await Tag.update(req.body, {
+      where: {
+        id: req.params.id,
+      },
+    });
+    if (!tagData) {
+      res.status(404).json({ message: 'No tag with this id!' });
+      return;
+    }
+    res.status(200).json(tagData);
+  } catch (err) {
+    res.status(500).json(err);
+  
+  }
 });
 
 router.delete('/:id', (req, res) => {

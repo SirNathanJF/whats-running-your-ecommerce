23cdff932de09033a3d6d8c48870630e85a028cc
Nathan Flessner <nathanflessner1@gmail.com>
2021-09-18 20:34:03 -0600

    added get method for a product_id in the product-routes.js

--- routes/api/product-routes.js
+++ routes/api/product-routes.js
@@ -33,9 +33,32 @@ router.get('/', async (req, res) => {
 });
 
 // get one product
-router.get('/:id', (req, res) => {
+router.get('/:id', async (req, res) => {
   // find a single product by its `id`
   // be sure to include its associated Category and Tag data
+  try {
+    const productData = await Product.findOne({
+      where: {
+        id: req.params.id
+      },
+      include: [{
+        model: Category,
+        attributes: ['category_name']
+      },
+      {
+        model: Tag,
+        attributes: ['tag_name']
+      }
+      ]
+    })
+    if (!productData) {
+      res.status(404).json({ message: 'No products found!' });
+      return;
+    }
+    res.status(200).json(productData);
+  }catch (err){
+    res.status(500).json(err);
+  }
 });
 
 // create new product
